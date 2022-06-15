62b31bc0bbe92074e05ca22e87d9f58ec2f3d39e
Carlos vouking <carlosvouking@gmail.com>
2022-06-15 10:49:09 +0200

    updated

--- FRONTEND/src/components/Navbar.jsx
+++ FRONTEND/src/components/Navbar.jsx
@@ -4,24 +4,36 @@ import {AiOutlineClose} from "react-icons/ai";
 import logo from "../../images/logo.png";   
 
 // functional component for items in the navbar
-const NavBarItem = ({ title, classProps }) => (
-  <li className={`mx-4 cursor-pointer ${classProps}`}> {title} </li>
-);
+const NavBarItem = ({ title, classProps }) => {
+  return (
+    <li className={`mx-4 cursor-pointer ${classProps}`}> 
+      {title} 
+    </li>
+  )
+};
 
 const Navbar = () => {
+  //  const [toggleMenu, setToggleMenu] = React.useState(false);
+
+
     return (
        <nav className="w-full flex md:justify-center justify-between items-center p-4">
         <div className="md:flex-[0.5] flex-initial justify-center items-center">
             <img src={logo} alt="logo" className="w-32 cursor-pointer" />
         </div>
         <ul className="text-yellow-400 md:flex hidden list-none flex-row justify-between items-center flex-initial">
-            {["Market", "Exchange", "Takaways", "Wallets"].map((item, index) => (
-            <NavBarItem key={item + index} title={item} />
+            {["Market", "Exchange", "Wallets"].map((item, index) => (
+               <NavBarItem key={item + index} title={item} />
             ))}
-            <li className="text-yellow-900 bg-[#d69e2e] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#b7791f] hover:text-yellow-300">
-              Login
+            <li className="text-yellow-900 bg-[#d69e2e] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#b7791f] hover:text-yellow-500">
+               Login
             </li>
         </ul>
+
+        {/* responsiveness...mobile screens */}
+        <div className="flex relative">
+           
+        </div>
        </nav>
     ); 
 }
